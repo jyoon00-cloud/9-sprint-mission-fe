9e634433e6d7e96be680c2fae100380566996b9f
jyoon00-cloud <ljyoon001261@gmail.com>
2025-11-04 09:09:45 +0900

    ✨  공용 컴포넌트

--- panda-market/src/components/ui/inputBox.jsx
+++ panda-market/src/components/ui/inputBox.jsx
@@ -1,3 +1,3 @@
 export default function InputBox({}) {
-  return <input className="flex bg-gray-100 w-full" type="text" />;
+  return <input className="flex bg-gray-100 w-full h-11" type="text" />;
 }
