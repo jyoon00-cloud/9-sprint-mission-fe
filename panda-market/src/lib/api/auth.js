2c558a3e8618ae3e62d1e8ee2862b2a688601962
jyoon00-cloud <ljyoon001261@gmail.com>
2025-11-13 11:27:19 +0900

    🎨 export 정리

--- panda-market/src/lib/api/auth.js
+++ panda-market/src/lib/api/auth.js
@@ -1,26 +1,19 @@
 import axios from "axios";
 
-const BASE_URL = " https://panda-market-api.vercel.app";
+const BASE_URL = "https://panda-market-api.vercel.app";
 const api = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
 });
 
-async function postSignUp(data = {}) {
+export async function postSignUp(data = {}) {
   const res = await api.post("/auth/signUp", data);
   return res.data;
 }
-async function postSignIn(data = {}) {
+export async function postSignIn(data = {}) {
   const res = await api.post("/auth/signIn", data);
   return res.data;
 }
-async function refreshToken() {
+export async function refreshToken() {
   const res = await api.post("/auth/refresh-token");
   return res.data;
 }
-
-export const authService = {
-  postSignUp,
-  postSignIn,
-  refreshToken,
-};
